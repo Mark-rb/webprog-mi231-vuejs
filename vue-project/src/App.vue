ad940b0ae35926e6f3f49ef53561697fa10201ca
Mark-rb <mobarbon@student.apc.edu.ph>
2025-02-02 16:57:34 +0800

    update

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,12 +1,18 @@
 <template>
 <h1>Food</h1>
-<food-item/> <br>
-<food-item2/> <br>
-<food-item/> <br>
-<personal-profile/> <br>
+<food-item/>
+<food-item/>
+<food-item/>
 </template>
  
 <script></script>
  
 <style>
-</style>           
+  #app > div {
+        border: dashed black 1px;
+        display: inline-block;
+        margin: 10px;
+        padding: 10px;
+        background-color: lightgreen;
+    }
+</style>        
--- vue-project/src/components/FoodItem.vue
+++ vue-project/src/components/FoodItem.vue
@@ -1,27 +1,19 @@
 <template>
-<div class="apple">
+<div>
 <h2>{{ name }}</h2>
 <p>{{ message }}</p>
 </div>
 </template>
+ 
 <script>
-  export default {
+export default {
     data() {
-      return {
-        name: 'Apples',
-        message: 'I like apples'
-      }
+        return {
+            name: 'Apples',
+            message: 'I like apples'
+        }
     }
-  };
+};
 </script>
-<style>
  
-.apple {
-        border: dashed black 1px;
-        display: inline-block;
-        margin: 10px;
-        padding: 10px;
-        background-color: lightgreen;
-    }
- 
-</style>
+<style></style>      
