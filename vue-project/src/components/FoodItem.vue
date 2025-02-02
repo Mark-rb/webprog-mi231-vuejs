40d74b9c7d65ca7058937daaf8135153e60c642b
Mark-rb <mobarbon@student.apc.edu.ph>
2025-02-02 17:06:20 +0800

    update

--- vue-project/src/components/FoodItem.vue
+++ vue-project/src/components/FoodItem.vue
@@ -1,19 +1,27 @@
 <template>
-<div>
+<div class="apple">
 <h2>{{ name }}</h2>
 <p>{{ message }}</p>
 </div>
 </template>
- 
 <script>
-export default {
+  export default {
     data() {
-        return {
-            name: 'Apples',
-            message: 'I like apples'
-        }
+      return {
+        name: 'Apples',
+        message: 'I like apples'
+      }
     }
-};
+  };
 </script>
+<style>
  
-<style></style>      
+.apple {
+        border: dashed black 1px;
+        display: inline-block;
+        margin: 10px;
+        padding: 10px;
+        background-color: lightgreen;
+    }
+ 
+</style>
