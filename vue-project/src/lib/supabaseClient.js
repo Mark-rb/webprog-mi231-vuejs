6a6c532ff3ac77190ff01bf76e72470167669457
Mark-rb <mobarbon@student.apc.edu.ph>
2025-02-11 13:31:32 +0000

    update

--- vue-project/src/lib/supabaseClient.js
+++ vue-project/src/lib/supabaseClient.js
@@ -1,3 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
  
   export const supabase = createClient('https://liukvslaiikpylheburl.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpdWt2c2xhaWlrcHlsaGVidXJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg1NjQ1MDQsImV4cCI6MjA1NDE0MDUwNH0.ATlD5sXtmwyStsb8TOiIlT1HELfltLOt14Uauz7X4CM')
+  
